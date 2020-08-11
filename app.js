846ae2beed19dec5ab020b10ca22908d1647b68c
uditsuthar <69505042+uditsuthar@users.noreply.github.com>
2020-08-11 14:11:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyA3wpIhZvlOTh18yJVRI4UxbGmysD0cu-Q",
+  authDomain: "prosuthar-2d228.firebaseapp.com",
+  databaseURL: "https://prosuthar-2d228.firebaseio.com",
+  projectId: "prosuthar-2d228",
+  storageBucket: "prosuthar-2d228.appspot.com",
+  messagingSenderId: "677488717299",
+  appId: "1:677488717299:web:a27866df324be8c87f7686",
+  measurementId: "G-X3V14EHRFS"
 };
 
      
